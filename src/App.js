cbb61ec56ac50b3f40b03eb04b197ceb4d055091
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-04-20 13:57:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import React, { useState, Fragment } from 'react';
-
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
-
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
-
-  return (
-    <Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </Fragment>
-  );
+  return <h2>Welcome</h2>
 }
 
 export default App;
